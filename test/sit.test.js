657d56e391e073cf663055fb154271a19cbf6a0f
taoyuan <torworx@gmail.com>
2015-07-24 11:31:33 +0800

    added injector as facet tests

--- test/sit.test.js
+++ test/sit.test.js
@@ -4,44 +4,54 @@ var t = require('chai').assert;
 var sit = require('../');
 
 describe('sit', function () {
-  it('should accept value facet', function () {
-    var injector = sit.injector({
-      foo: ['value', 100]
+
+  describe('injector', function () {
+    it('should accept value facet', function () {
+      var injector = sit.injector({
+        foo: ['value', 100]
+      });
+
+      t.equal(injector.get('foo'), 100);
     });
 
-    t.equal(injector.get('foo'), 100);
-  });
-
-  it('should access builtin $options facet', function () {
-    var injector = sit.injector({
-      foo: function ($options) {
-        return $options.foo;
-      }
-    }, {foo: 'bar'});
-
-    t.equal(injector.get('foo'), 'bar');
-  });
-
-  it('should access builtin $logs facet', function () {
-    var injector = sit.injector();
-    t.ok(injector.get('$logs'));
-  });
-
-  it('should throw error with full path if no provider', function () {
-    var injector = sit.injector({
-      a: function (b) {
-        return 'a-value';
-      },
-      b: function (c) {
-        return 'b-value';
-      }
+    it('should get injector self', function () {
+      var injector = sit.injector();
+      injector.invoke(function (injector) {
+        t.ok(injector);
+        t.isFunction(injector.invoke)
+      });
     });
 
-    t.throw(function () {
-      injector.get('a');
-    }, 'No provider for "c"! (Resolving: a -> b -> c)');
-  });
+    it('should access builtin $options facet', function () {
+      var injector = sit.injector({
+        foo: function ($options) {
+          return $options.foo;
+        }
+      }, {foo: 'bar'});
 
+      t.equal(injector.get('foo'), 'bar');
+    });
+
+    it('should access builtin $logs facet', function () {
+      var injector = sit.injector();
+      t.ok(injector.get('$logs'));
+    });
+
+    it('should throw error with full path if no provider', function () {
+      var injector = sit.injector({
+        a: function (b) {
+          return 'a-value';
+        },
+        b: function (c) {
+          return 'b-value';
+        }
+      });
+
+      t.throw(function () {
+        injector.get('a');
+      }, 'No provider for "c"! (Resolving: a -> b -> c)');
+    });
+  });
 
   describe('facets/kvstore', function () {
 
