b0fe058c09cca0e497bc39f19e5cd810b7ba97ce
taoyuan <torworx@gmail.com>
2015-07-27 23:31:29 +0800

    added protocol support for jsonrpc facet

--- test/facets/jsonrpc.test.js
+++ test/facets/jsonrpc.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var t = require('chai').assert;
+var s = require('../support')
 var sit = require('../../');
 var request = require('supertest');
 
@@ -38,7 +39,7 @@ describe('facets/jsonrpc', function () {
       t.notOk(server.methods['_private_method']);
       t.notOk(server.methods['name']);
       server.$promise.finally(function () {
-        server.httpServer.close(done);
+        server.server.close(done);
       });
     });
 
@@ -65,7 +66,7 @@ describe('facets/jsonrpc', function () {
           .expect({status: 'ok'})
           .end(function (err) {
             if (err) throw err;
-            server.httpServer.close(done);
+            server.server.close(done);
           });
       });
     });
@@ -134,7 +135,7 @@ describe('facets/jsonrpc', function () {
           t.equal(answer, 'ping');
           t.equal(response, 'pong');
         }).finally(function () {
-          server.httpServer.close(done);
+          server.server.close(done);
         });
       });
     });
@@ -179,7 +180,7 @@ describe('facets/jsonrpc', function () {
         fooClient.request('bar', 'ping').then(function (response) {
           t.equal(answer, 'ping');
         }).finally(function () {
-          server.httpServer.close(done);
+          server.server.close(done);
         });
       });
     });
@@ -230,7 +231,60 @@ describe('facets/jsonrpc', function () {
           t.equal(err.code, 403);
           t.equal(err.message, 'Verification of signed message failed');
         }).finally(function () {
-          server.httpServer.close(done);
+          server.server.close(done);
+        });
+      });
+    });
+  });
+
+  describe('protocol/mqtt', function () {
+
+    before(s.abStartMosca());
+    after(s.abStopMosca());
+
+    it('should work with mqtt protocol', function (done) {
+      var answer;
+      var foo = {
+        bar: function (msg) {
+          answer = msg;
+          return 'pong';
+        }
+      };
+
+      var serverContainer = sit.container({
+        foo: foo,
+        server: sit.facets.jsonrpc.server('foo')
+      }, {
+        server: {
+          protocol: 'mqtt',
+          port: s.port,
+          topic: '$rpc',
+          signing: {
+            sign: false
+          }
+        }
+      });
+      var server = serverContainer.get('server');
+
+      var clientContainer = sit.container({
+        foo: sit.facets.jsonrpc.client()
+      }, {
+        foo: {
+          protocol: 'mqtt',
+          port: s.port,
+          topic: '$rpc',
+          signing: {
+            sign: false
+          }
+        }
+      });
+      var fooClient = clientContainer.get('foo');
+
+      server.$promise.then(function () {
+        fooClient.request('bar', 'ping').then(function (response) {
+          t.equal(answer, 'ping');
+        }).finally(function () {
+          server.server.close(done);
         });
       });
     });
---
+++ test/support.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var _ = require('lodash');
+var mosca = require('mosca');
+
+var port = 6677;
+
+Object.defineProperty(exports, 'port', {
+  get: function () {
+    return port;
+  }
+});
+
+exports.startMosca = function (settings) {
+  settings = _.assign({port: port}, settings);
+  console.log();
+  console.log('start mosca', settings);
+  console.log();
+  return new mosca.Server(settings);
+};
+
+exports.abStartMosca = function (settings) {
+  return function (done) {
+    this.mosca = exports.startMosca(settings);
+    this.mosca.on('ready', done);
+  }
+};
+
+exports.abStopMosca = function () {
+  return function(done) {
+    console.log();
+    console.log('close mosca');
+    console.log();
+    if (this.mosca) return this.mosca.close(done);
+    done();
+  }
+};
