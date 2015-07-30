88876587aa2e9f86a1c809ca815b538fd71a326d
taoyuan <torworx@gmail.com>
2015-07-30 11:56:28 +0800

    added metrics using statasd-client

---
+++ test/facets/metrics.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var t = require('chai').assert;
+var sit = require('../../');
+
+describe('facets/metrics', function () {
+
+  it('should initialize statsd client', function () {
+    var container = sit.container({
+      metrics: sit.facets.metrics()
+    });
+    var metrics = container.get('metrics');
+    t.ok(metrics);
+    metrics.close();
+  });
+
+  describe('operations', function () {
+
+    var container, metrics;
+    before(function () {
+      container = sit.container({
+        metrics: sit.facets.metrics()
+      }, {
+        metrics: {
+          debug: true
+        }
+      });
+
+      metrics = container.get('metrics');
+    });
+
+    after(function () {
+      metrics.close();
+    });
+
+    it.only('should count', function () {
+      metrics.increment('foo', 1);
+    });
+  });
+
+
+});
